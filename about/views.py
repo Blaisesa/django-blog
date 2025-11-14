a0ee075cb8da8ac92d7680055c608454bc414705
blaise <blaise.tf01@gmail.com>
2025-11-14 13:49:53 +0000

    add about page

---
+++ about/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import About
+
+
+# Create your views here.
+def about_view(request):
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render(request, 'about/about.html', {'about': about})
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path("about/", include("about.urls"), name="about-urls"),
     path('', views.PostList.as_view(), name='home'),
+    path('summernote/', include('django_summernote.urls')),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
